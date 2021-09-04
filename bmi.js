c0ac99a7709af67413eeb27452192e488907453f
ahmadmikail <50962065+ahmadmikail@users.noreply.github.com>
2021-09-04 13:53:14 +0700

    Initial commit

---
+++ bmi.js
@@ -0,0 +1,18 @@
+ hitungBmi = () => {
+	var weight = document.getElementById('weight').value;
+	var height = document.getElementById('height').value;
+ 
+	if(weight > 0 && height > 0){	
+		var bmi = weight/(height/100*height/100);
+		document.getElementById('bmi').value = bmi;
+ 
+        let result = 
+        bmi < 18.5 ? document.getElementById('result').value = "Terlalu kurus" :
+        bmi < 24.9 ? document.getElementById('result').value = "Normal" :
+        document.getElementById('result').value = "Terlalu gemuk";
+		
+	}
+	else{
+		alert("Please enter something first!")
+	}
+}
